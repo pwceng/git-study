8abc156fc4783021e2c596d79a45451390d37f95
525547095@qq.com <525547095@qq.com>
2021-04-11 17:16:36 +0800

    branch02--第一次提交

--- study01/src/main/java/com/study/GItStudy.java
+++ study01/src/main/java/com/study/GItStudy.java
@@ -9,5 +9,7 @@ public class GItStudy {
     public static void main(String[] args) {
         System.out.println("main--第一次提交");
         System.out.println("main --第二次提交");
+
+        System.out.println("main主干更新");
     }
 }
